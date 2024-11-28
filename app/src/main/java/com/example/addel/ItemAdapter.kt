5a96f67835d84250018b0f0001d73217f4afa8c6
Almond1400 <mpcknna@gmail.com>
2024-11-28 16:47:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/addel/ItemAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.addel
+
+import android.media.RouteListingPreference.Item
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter(private val item: List<item>) :
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView :TextView = view.findViewById(R.id.txt1)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            ItemViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_rec, parent, false)
+        return ItemViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.textView.text = item[position].text
+    }
+    override fun getItemCount(): Int {
+        return item.size
+    }
+}
---
+++ app/src/main/java/com/example/addel/item.kt
@@ -0,0 +1,5 @@
+package com.example.addel
+
+data class item(
+    val text: String
+)
